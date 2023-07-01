9fce2fbf2464b5c59865df18f03de51d3dc1b545
Owen Shi <2008owenshi@gmail.com>
2023-07-01 10:17:59 -0400

    fix some table bugs, add boilerplate for assignments table

---
+++ app/components/Table.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
+
+export default function Copyright({
+  title,
+  keys,
+  children,
+  count,
+  onPageChange,
+  page,
+  rowsPerPage,
+  rowsPerPageOptions = [],
+}: {
+  title: string;
+  keys: string[];
+  children: React.ReactNode;
+  count: number;
+  onPageChange: () => void;
+  page: number;
+  rowsPerPage: number;
+  rowsPerPageOptions?: number[];
+}) {
+  return (
+    <TableContainer component={Paper}>
+      <Typography sx={{ fontWeight: 600, px: 2, pt: 2 }} variant='h6'>
+        {title}
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {keys.map((key) => (
+              <TableCell sx={{ fontSize: 12 }}>{key}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>{children}</TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              count={count}
+              onPageChange={onPageChange}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              rowsPerPageOptions={rowsPerPageOptions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
--- app/dashboard/classes/[id]/page.tsx
+++ app/dashboard/classes/[id]/page.tsx
@@ -1,5 +1,177 @@
-// TODO: Make class detail UI
+'use client';
+
+import dynamic from 'next/dynamic';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Table from '@/app/components/Table';
+
+const students = [
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    id: 'c451bd0d-0327-46c6-adfc-42f57514c674',
+    grade: 96,
+  },
+];
 
 export default function ClassDetail({ params }: { params: { id: string } }) {
-  return <h1>{params.id}</h1>;
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Paper sx={{ p: 2 }}>
+            <Typography sx={{ fontWeight: 500 }} variant='body1'>
+              AVERAGE CLASS PERFORMANCE{' '}
+            </Typography>
+            <Typography variant='h5' sx={{ mt: 1 }}>
+              90%
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper sx={{ p: 2 }}>
+            <Typography sx={{ fontWeight: 500 }} variant='body1'>
+              AVERAGE CLASS PERFORMANCE{' '}
+            </Typography>
+            <Typography variant='h5' sx={{ mt: 1 }}>
+              90%
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper sx={{ p: 2 }}>
+            <Typography sx={{ fontWeight: 500 }} variant='body1'>
+              AVERAGE CLASS PERFORMANCE{' '}
+            </Typography>
+            <Typography variant='h5' sx={{ mt: 1 }}>
+              90%
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper sx={{ p: 2 }}>
+            <Typography sx={{ fontWeight: 500 }} variant='body1'>
+              AVERAGE CLASS PERFORMANCE{' '}
+            </Typography>
+            <Typography variant='h5' sx={{ mt: 1 }}>
+              90%
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={5}>
+          <Table
+            title='Students'
+            keys={['NAME', 'EMAIL', 'GRADE']}
+            count={students.length}
+            onPageChange={() => ''}
+            page={0}
+            rowsPerPage={5}
+          >
+            {students.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell>{student.name}</TableCell>
+                <TableCell>
+                  <Link href={`mailto:${student.email}`}>{student.email}</Link>
+                </TableCell>
+                <TableCell>
+                  <Box
+                    component='span'
+                    sx={{
+                      bgcolor: 'lightgreen',
+                      p: '7px',
+                      borderRadius: 4,
+                      color: 'green',
+                      fontWeight: 600,
+                    }}
+                  >
+                    {student.grade}%
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </Table>
+        </Grid>
+        <Grid item xs={7}>
+          <Table
+            title='Assignments'
+            keys={['NAME', 'EMAIL', 'GRADE']}
+            count={students.length}
+            onPageChange={() => ''}
+            page={0}
+            rowsPerPage={5}
+          >
+            {students.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell>{student.name}</TableCell>
+                <TableCell>
+                  <Link href={`mailto:${student.email}`}>{student.email}</Link>
+                </TableCell>
+                <TableCell>
+                  <Box
+                    component='span'
+                    sx={{
+                      bgcolor: 'lightgreen',
+                      p: '7px',
+                      borderRadius: 4,
+                      color: 'green',
+                      fontWeight: 600,
+                    }}
+                  >
+                    {student.grade}%
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </Table>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
