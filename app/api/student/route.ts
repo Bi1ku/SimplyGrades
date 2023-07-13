7ae98179ba42d5f4957d5ce3eb70cfe014956cf9
Owen Shi <2008owenshi@gmail.com>
2023-07-13 18:35:01 -0400

    init routes

---
+++ app/api/student/route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server'
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+}
+
+export async function POST() {
+}
+
+export async function PUT() {
+}
+
+export async function DELETE() {
+}
